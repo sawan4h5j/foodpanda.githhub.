6a283241b157e7bdb3862c1d6d7b06f843004a87
sawan4h5j <154305330+sawan4h5j@users.noreply.github.com>
2023-12-20 15:25:43 +0600

    Add files via upload

---
+++ file1.js
@@ -0,0 +1,57 @@
+console.log ('Hello world')
+
+var Friend Name='foridul islam';
+console.log(Friend Name)
+
+var stytype(sawan programmar);
+
+console.log(typeof strtype)
+
+var numaType = 76237356473;
+
+console.log (typeof numaType)
+
+var Booltype = true;
+
+console.log(typeof Booltype)
+
+var rochona="Lorem ipsum1 s th ty tyht h th  th th  th th tr h th t h
+tghg hghstgdh tsyht yhtg h ths th ts yht yhtyhtyht tyy ty  ytry thyhhy"
+
+console.log(rochona.toUpperCase())
+console.log(rochona.tolowerCase())
+
+var myfnd = "Forid Mohin Tasin Siam Sawan"
+
+console.log(my Friend.split(forid));
+
+console.log(myfnd.indexOf("M"))
+
+var num1 = "55";
+
+var num2 = 99;
+
+var mot = num1 + num2;
+console.log(mot);
+ var num3 = 20;
+ var num4 = 90;
+
+ num4 = "" + num4;
+ var total = num3 + num4;
+
+ console.log(total)
+
+ var num1 = 60;
+ var num2 = 70;
+
+ console.log(num1 * num2)
+
+ var num1 = -20;
+
+ var sum = Math.abs(num1)
+ console.log(sum)
+
+ var voter = 0.99999;
+
+ var sum = math.round/cli/floor/random(voter)
+ console.log(sum)
---
+++ if.js
@@ -0,0 +1,44 @@
+var a = 2;
+var b = 1;
+
+if( a < b){
+    console.log("see your not for mind")
+}
+
+var a = 0 
+var b = 1
+
+if(a > b){
+    console.log("Hello world")}
+    else {
+        console.log("hello world 2")
+    }
+var x = 500;
+var y = 400'
+
+if(x > y){
+    console.log("x boro");
+}
+else if(x > y){
+    console.log("x boro");
+
+else{
+    console.log("they both are same")
+}
+var sum = "hello world"
+
+switch(sum){
+case 10:
+
+console.log("hello world");
+break;
+case 1:
+
+console.log("i am a1");
+case 5:
+    console.log("i am a 5");
+    break;
+    default:
+        console.log("ami tomake khoje pelam na");
+        
+}
